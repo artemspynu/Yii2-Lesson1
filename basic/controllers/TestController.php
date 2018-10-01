e4e55144863e553f23429fc97bd01c9fb47bc672
artem spynu <artem-lider@mail.ru>
2018-10-01 15:03:49 +0300

    Выполнил с первого по девятый пункты ДЗ

---
+++ basic/controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Product;
+use yii\web\Controller;
+
+
+class TestController extends Controller
+{
+    public function actionIndex()
+    {
+        $model = new Product();
+        $model -> id = 1;
+        $model -> name = 'Good';
+        $model -> price = 123;
+
+        return $this->render('index', [
+            'model' => $model
+        ]);
+
+        return $this->render('index', [
+            'var' => 'Hello World!'
+        ]);
+    }
+}
+
---
+++ basic/views/test/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var string $var
+ * @var \app\models\Product $model
+ */
+?>
+<h1><?= $var ?></h1>
+<h1><?= $model->id ?></h1>
+<?= \yii\widgets\DetailView:: widget(['model' => $model])?>
+
+<?php
